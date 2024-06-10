486296bb1bdb76826486b71e755fcf5bd5d79e28
Bryan Reed <bryan87reed@gmail.com>
2024-06-09 22:48:19 -0700

    lower volume

--- scripts/global.gd
+++ scripts/global.gd
@@ -55,7 +55,7 @@ func play_dialogue_sfx(parent, laugh = false):
 	var sfx_player = AudioStreamPlayer.new()
 	randomize()
 	var track_num = randi_range(1, 10)
-	#sfx_player.volume_db = db_override
+	sfx_player.volume_db = -5
 	sfx_player.stream = load("res://sfx/Dialogue/Dialogue - Villain Expo "+str(track_num)+".ogg")
 	if laugh:
 		sfx_player.finished.connect(play_dialogue_laugh_sfx.bind(sfx_player))
@@ -68,7 +68,7 @@ func play_dialogue_sfx(parent, laugh = false):
 func play_dialogue_laugh_sfx(sfx_player):
 	randomize()
 	var track_num = randi_range(1, 2)
-	#sfx_player.volume_db = db_override
+	sfx_player.volume_db = -5
 	sfx_player.stream = load("res://sfx/Dialogue/Dialogue - Villain Laugh "+str(track_num)+".ogg")
 	sfx_player.finished.connect(sfx_player.queue_free)
 	sfx_player.play()
